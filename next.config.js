f70f1d9b973ab4c76e164d08a8105d568086528b
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2023-02-03 00:26:38 -0300

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
+
+// pageExtensions - all files and in page.tsx now is a route path.
